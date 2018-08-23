7b0ef15010ad26eb297327750e32edcd889be2b8
Halljamie18 <jamie.hall@blis.com>
2018-08-23 16:53:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file.
+
+let drivers = ['bobby', 'sammy', 'sally', 'annette', 'sarah', 'bobby']
+
+function studentPatronusName(student) {
+  return student.namePatronus();
+}
+ 
+let students = [harryPotter, ronWeasley, hermioneGranger, ginevraWeasley];
+let patroni = students.map(studentPatronusName);
